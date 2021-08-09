d531a0cf9d4d78d6a58712fea3b44bb82e5699ab
Tudny <aleksanderwt@gmail.com>
2021-08-10 00:49:02 +0200

    clean up

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -1,6 +1,8 @@
-//
-// Created by aleks on 2/24/2020.
-//
+/** @file
+ * Brainf*ck interpreter module implementation.
+ * @author Aleksander "Tudny" Tudruj
+ * @date 24/02/2020
+ * */
 
 #include "brainfuck.h"
 #include <string>
@@ -8,83 +10,105 @@
 
 namespace brainfuck {
 
-    void print_data(int* tab, int __size){
-        for(int i = 0; i < __size; i++) {
+    /**
+     * Prints whole table of ints.
+     * @param[in] tab : pointer to a table with ints
+     * @param[in] _size : length of printed table
+     * */
+    void print_data(const int* const tab, const int _size) {
+        for (int i = 0; i < _size; i++)
             std::cout << i << " ";
-        }
 
         std::cout << "\n";
 
-        for(int i = 0; i < __size; i++){
+        for (int i = 0; i < _size; i++)
             std::cout << tab[i] << " ";
-        }
+
         std::cout << "\n";
     }
 
-    bool bracket_check(const std::string &input){
+    /**
+     * Checks the correction of the brackets.
+     * Function checks the correction of the brackets in the Brainf*ck code.
+     * If any bracket is missing or the bracketing is incorrect the function
+     * returns false otherwise true.
+     * @param[in] input : checked code
+     * @return if the bracketing is correct
+     * */
+    bool bracket_check(const std::string &input) {
         int cnt = 0;
-        for(char c : input){
-            if(cnt < 0) return false;
-            if(c == '[') cnt++;
-            if(c == ']') cnt--;
+        for (char c : input) {
+            if (cnt < 0) return false;
+            else if (c == '[') cnt++;
+            else if (c == ']') cnt--;
         }
-        return (cnt == 0);
+        return cnt == 0;
     }
 
     void interpret(const std::string &input, bool debug) {
-        signed __size = (1u << 5u);
-        int* tab = new int[__size];
-        std::fill(tab, tab + __size, 0);
+        if (!bracket_check(input))
+            throw bad_bracketing();
+
+        signed _size = BUFFER_SIZE;
+        int* tab = new int[_size];
+        std::fill(tab, tab + _size, 0);
         int* p = tab;
 
-        if(!bracket_check(input)){
-            throw std::runtime_error("Bracket match error");
-        }
-
-        for(int i = 0 ; i < input.size(); i++){
+        for (int i = 0; i < input.size(); i++) {
             char c = input[i];
 
-            if(debug) print_data(tab, __size);
+            if (debug)
+                print_data(tab, _size);
 
-            if(c == '+'){
+            if (c == '+') {
                 (*p)++;
-            } else if(c == '-'){
+            } else if(c == '-') {
                 (*p)--;
-            } else if(c == '>'){
+            } else if(c == '>') {
                 p++;
-            } else if(c == '<'){
+            } else if(c == '<') {
                 p--;
-            } else if(c == '.'){
-                std::cout << (char)(*p) << "";
-            } else if(c == ','){
+            } else if(c == '.') {
+                std::cout << (char)(*p);
+            } else if(c == ',') {
                 std::cin >> (*p);
-            } else if(c == '['){
-                if((*p) == 0){
+            } else if(c == '[') {
+                if (*p == 0) {
                     int cnt = 0;
                     i++;
-                    while(i < input.length()){
-                        if(input[i] == ']' && cnt == 0) break;
-                        if(input[i] == '[') cnt++;
-                        if(input[i] == ']') cnt--;
+                    while (i < input.length()) {
+                        if (input[i] == ']' && cnt == 0) break;
+                        if (input[i] == '[') cnt++;
+                        if (input[i] == ']') cnt--;
                         i++;
                     }
                 }
-            } else if(c == ']'){
-                if((*p) != 0){
+            } else if(c == ']') {
+                if ((*p) != 0) {
                     int cnt = 0;
                     i--;
-                    while(i >= 0){
-                        if(input[i] == '[' && cnt == 0) break;
-                        if(input[i] == ']') cnt++;
-                        if(input[i] == '[') cnt--;
+                    while (i >= 0) {
+                        if (input[i] == '[' && cnt == 0) break;
+                        if (input[i] == ']') cnt++;
+                        if (input[i] == '[') cnt--;
                         i--;
                     }
                 }
-            } else if(c == ' '){
+            } else if(c == ' ') {
                 continue;
             } else {
-                throw std::runtime_error("Bad character");
+                throw bad_character();
             }
         }
+    }
+
+    const char *
+    bad_bracketing::what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW {
+        return "Bad bracketing!";
+    }
+
+    const char *
+    bad_character::what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW {
+        return "Bad character!";
     }
 }
--- src/brainfuck.h
+++ src/brainfuck.h
@@ -1,6 +1,8 @@
-//
-// Created by aleks on 2/24/2020.
-//
+/** @file
+ * Brainf*ck interpreter module.
+ * @author Aleksander "Tudny" Tudruj
+ * @date 24/02/2020
+ * */
 
 #ifndef BF_BRAINFUCK_H
 #define BF_BRAINFUCK_H
@@ -8,6 +10,36 @@
 #include <string>
 
 namespace brainfuck {
+
+    const int BUFFER_SIZE = 1u << 5u; ///< Default size of a buffer.
+
+    /**
+     * Exception thrown when the bracketing of the code is wrong.
+     * */
+    class bad_bracketing : public std::exception {
+    public:
+        const char *
+        what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW override;
+    };
+
+    /**
+     * Exception thrown when there is a wrong character in the code.
+     * */
+    class bad_character : public std::exception {
+    public:
+        const char *
+        what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW override;
+    };
+
+    /**
+     * Function interpreting Brainf*ck code.
+     * Brainf*ck code is being interpreted and all the results are being
+     * printed to std::out.
+     * @param[in] input : pointer to the string with Brainf*ck code
+     * @param[in] debug : flag enabling printing the whole Brainf*ck's table
+     * @throws brainfuck::bad_bracketing when the bracketing is incorrect
+     * @throws brainfuck::bad_character when there is one
+     * */
     void interpret(const std::string &input, bool debug = false);
 }
 
